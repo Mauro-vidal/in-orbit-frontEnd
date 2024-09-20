723b90cde7ab45e0ff4aef79777568e0854020ad
Mauro-vidal <mauroviidal@gmail.com>
2024-09-20 16:22:08 -0300

    requisição http get-summary

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,13 @@ import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
 import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 // import { Summary } from './components/summary'
 
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
-
 export function App() {
-  const { data } = useQuery<SummaryResponse>({
+  const { data } = useQuery({
     queryKey: ['summary'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3333/summary')
-      const data = await response.json()
-
-      return data.summary
-    },
+    queryFn: getSummary,
   })
 
   return (
