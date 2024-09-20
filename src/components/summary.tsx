9700cee011febfe229241047d219e7f6091bb374
Mauro-vidal <mauroviidal@gmail.com>
2024-09-20 17:24:01 -0300

    listagem de metas concluidas na semana com hora.

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -99,14 +99,19 @@ export function Summary() {
               </h3>
 
               <ul className="flex flex-col gap-3">
-                <li className="flex items-center gap-2">
-                  <CheckCircle2 className="size-4 text-pink-500" />
-                  <span className="text-sm text-zinc-400">
-                    Você completou "
-                    <span className="text-zinc-100">Acordar cedo</span>" às{' '}
-                    <span className="text-zinc-100"> 08:13h</span>
-                  </span>
-                </li>
+                {goals.map(goal => {
+                  const time = dayjs(goal.completedAt).format('HH:mm')
+                  return (
+                    <li key={goal.id} className="flex items-center gap-2">
+                      <CheckCircle2 className="size-4 text-pink-500" />
+                      <span className="text-sm text-zinc-400">
+                        Você completou "
+                        <span className="text-zinc-100">{goal.title}</span>" às{' '}
+                        <span className="text-zinc-100">{time}h</span>
+                      </span>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           )
