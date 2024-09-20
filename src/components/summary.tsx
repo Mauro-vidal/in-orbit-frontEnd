db91d29ae2168453a11adbfdd52d89426d957c3a
Mauro-vidal <mauroviidal@gmail.com>
2024-09-20 12:24:02 -0300

    arrumando logo

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,12 +1,17 @@
 import { Plus } from 'lucide-react'
 import { Button } from './ui/button'
 import { DialogTrigger } from './ui/dialog'
+import { InOrbitIcon } from './in-orbit-icon'
 
 export function Summary() {
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
-        <span className="text-lg font-semibold">5 a 10 de agosto</span>
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold">5 a 10 de agosto</span>
+        </div>
+
         <DialogTrigger asChild>
           <Button size="sm">
             <Plus className="size-4" />
