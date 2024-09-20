afd9888089ba53920c07931ee44d255f2a9e2c99
Mauro-vidal <mauroviidal@gmail.com>
2024-09-20 15:50:24 -0300

    inicio de integração com back-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
-import { Dialog } from './components/ui/dialog'
+import { useEffect, useState } from 'react'
 
+import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
-// import { EmptyGoals } from './components/empty-goals'
+import { EmptyGoals } from './components/empty-goals'
+// import { Summary } from './components/summary'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setSummary(data.summary)
+      })
+  }, [])
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
 
+      {/* <EmptyGoals /> */}
+      {/* <Summary /> */}
       <CreateGoal />
     </Dialog>
   )
